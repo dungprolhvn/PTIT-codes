aeaa3882903fc95fc4f0eb901dd2cd5ff03b86fd
Dung Dinh <0965dung@gmail.com>
2024-01-31 16:53:18 +0700

    "Fix DSA06011.cpp to AC"

--- dsa/DSA06011.cpp
+++ dsa/DSA06011.cpp
@@ -12,7 +12,6 @@ int main()
         cin >> n;
         int a[n];
         for (int i = 0; i < n; i++) cin >> a[i];
-        sort(a, a + n);
         int min = INT_MAX;
         for (int i = 0; i < n; i++) {
             for (int j = i+1; j < n; j++) {
